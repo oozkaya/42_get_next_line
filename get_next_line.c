8dad13334025904326982c002e83aec29d1f8964
Onur OZKAYA <oozkaya@e3r6p14.42.fr>
2017-12-14 12:22:46 +0100

    ajout du MALLCHECK

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: oozkaya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 13:08:56 by oozkaya           #+#    #+#             */
-/*   Updated: 2017/12/07 09:28:03 by oozkaya          ###   ########.fr       */
+/*   Updated: 2017/12/14 12:22:26 by oozkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static t_list	*ft_check_fd(t_list **begin, int fd)
 
 int				get_next_line(int const fd, char **line)
 {
-	static t_list	*file;
+	static t_list	*f;
 	char			buf[BUFF_SIZE + 1];
 	int				ret;
 	t_list			*begin;
@@ -39,19 +39,19 @@ int				get_next_line(int const fd, char **line)
 
 	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
 		return (-1);
-	begin = file;
-	file = ft_check_fd(&begin, fd);
-	while (!ft_memchr(file->content, '\n', ft_strlen(file->content) + 1)
+	begin = f;
+	f = ft_check_fd(&begin, fd);
+	while (!ft_memchr(f->content, '\n', ft_strlen(f->content) + 1)
 			&& (ret = read(fd, buf, BUFF_SIZE)))
-		file->content = ft_strnjoinfree(file->content, buf, ret, 0);
+		MALLCHECK((f->content = ft_strnjoinfree(f->content, buf, ret, 0)));
 	ret = 0;
-	while (((char*)file->content)[ret] && ((char*)file->content)[ret] != '\n')
+	while (((char*)f->content)[ret] && ((char*)f->content)[ret] != '\n')
 		++ret;
-	*line = ft_strndup(file->content, ret);
-	(((char*)file->content)[ret] == '\n') ? ++ret : 0;
-	tmp = file->content;
-	file->content = ft_strdup(file->content + ret);
+	MALLCHECK((*line = ft_strndup(f->content, ret)));
+	(((char*)f->content)[ret] == '\n') ? ++ret : 0;
+	tmp = f->content;
+	MALLCHECK((f->content = ft_strdup(f->content + ret)));
 	free(tmp);
-	file = begin;
+	f = begin;
 	return (ret ? 1 : 0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: oozkaya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 13:11:57 by oozkaya           #+#    #+#             */
-/*   Updated: 2017/12/07 15:45:18 by oozkaya          ###   ########.fr       */
+/*   Updated: 2017/12/14 12:20:48 by oozkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-# define BUFF_SIZE 8000
+# define MALLCHECK(x) if(!x) return(-1);
+# define BUFF_SIZE 42
 
 int		get_next_line(const int fd, char **line);
 
